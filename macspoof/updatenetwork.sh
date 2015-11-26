f76d03424a562a5ee821112c5f85887a62c9c710
James Casey <hanavi@gmail.com>
2015-11-26 01:00:08 -0600

    Added OSX MAC spoofing script

---
+++ macspoof/updatenetwork.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Quick script to generate a random MAC address
+# This could probably be cleaner....
+
+## Dell and Lenovo from some website...
+START=( f8:cf:c5 ec:89:f5 e8:91:20 e4:90:7e e0:2c:b2 d8:71:57 d4:22:3f \
+        cc:07:e4 c8:dd:c9 ac:38:70 a4:8c:db a0:32:99 98:ff:d0 88:70:8c \
+        84:10:0d 80:cf:41 74:04:2b 70:72:0d 6c:5f:1c 60:d9:a0 60:99:d1 \
+        5c:51:88 50:3c:c4 44:80:eb 24:da:9b 20:76:93 1c:56:fe 14:9f:e8 \
+        14:36:c6 00:59:07 00:12:fe 00:06:1b f8:db:88 f8:ca:b8 f8:bc:12 \
+        f8:b1:56 f0:4d:a2 f0:1f:af ec:f4:bb e0:db:55 d4:be:d9 d4:ae:52 \
+        d0:67:e5 d0:43:1e c8:1f:66 bc:30:5b b8:ca:3a b8:ac:6f b8:2a:72 \
+        b0:83:fe a4:ba:db a4:1f:72 98:90:96 90:b1:1c 84:8f:69 84:2b:bc \
+        84:2b:2b 78:45:c4 78:2b:cb 74:e6:e2 74:86:7a 64:00:6a 5c:f9:dd \
+        5c:26:0a 54:9f:35 4c:76:25 44:a8:42 34:e6:d7 34:17:eb 28:c8:25 \
+        24:b6:fd 24:6e:96 20:47:47 1c:40:24 18:fb:7b 18:a9:9b 18:03:73 \
+        14:fe:b5 14:18:77 10:98:36 00:c0:4f 00:b0:d0 00:26:b9 00:25:bd \
+        00:25:64 00:24:e8 00:23:ae 00:22:19 00:21:9b 00:21:70 00:1e:c9 \
+        00:1e:4f 00:1d:09 00:1c:23 00:1a:a0 00:19:b9 00:18:8b 00:16:f0 \
+        00:15:c5 00:14:22 00:13:72 00:12:3f 00:11:43 00:0f:1f 00:0d:56 \
+        00:0b:db 00:08:74 00:06:5b )
+
+n=$(python -c "import random; print(random.randint(0,107))")
+END=$(openssl rand -hex 3 | sed 's/\(..\)/\1:/g; s/.$//')
+NEWMAC="${START[${n}]}:${END}"
+
+echo ""
+echo ""
+echo "Changing MAC address: ${NEWMAC}"
+sudo /sbin/ifconfig en0 ether ${NEWMAC}
+sudo /sbin/ifconfig en0 down
+sleep 2
+sudo /sbin/ifconfig en0 up
+check=$(ifconfig en0 |grep ether |awk '{print $2}')
+
+echo "Verify MAC:           ${check}"
+echo ""
+echo "-----------------------------------------------------"
+echo "NOTE: if the both addresses are not the same, you may"
+echo "need to rerun the script."
+echo "-----------------------------------------------------"
+echo ""
+echo ""
+
+read -p 'Press Enter to continue...'
+
