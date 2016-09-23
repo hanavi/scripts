dedabe0ba23d3301e3b87532939ca51248b4c193
James Casey <hanavi@gmail.com>
2016-09-23 08:40:29 -0500

    Cleaning up some test scripts

--- misc/motherless.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DLIST=$(curl ${1} | grep _self |awk '{print $2}' | awk 'BEGIN{FS="/"}{print $4}'|sed 's/"//g')
-
-for name in $DLIST
-do
-  curl http://cdn4.videos.motherlessmedia.com/videos/${name}.mp4 > ${name}.mp4
-done
